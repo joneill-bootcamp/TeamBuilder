a6b27866a0ea24ef4bf50afb07430d62f74f3ed4
John O'Neil <fluffywestie@tpg.com.au>
2020-06-14 21:15:53 +1000

    Update to REPO

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# TeamBuilder
-A Team Builder App using Node
+# Team Builder
+
+# Description
+
+Allows you to track your Teams
+
+# Repo Link
+
+> https://github.com/joneill-bootcamp/workouttracker
+
+# Heroku Link
+
+> https://aqueous-bastion-17761.herokuapp.com
+
+# Table of Contents
+
+1. [Installation](##Installation)
+2. [Usage](##Usage)
+3. [License](##License)
+4. [Technology](##Technology)
+5. [Contributing](##Contributing)
+6. [Tests](##Tests)
+7. [Questions](##Questions)
+
+## Installation
+
+Clone the repo with the following command
+
+> git clone https://github.com/joneill-bootcamp/workouttracker
+
+Run the following command
+
+> npm install
+
+## Usage
+
+To execute this tool
+
+> npm run start
+
+## Technology
+
+This project uses the following technology
+
+> HTML 5
+> JQuery
+> Express
+> Express-Session
+> MongoDB
+> BootStrap
+
+## License
+
+This project is licensed under the terms of the **MIT** license.
+
+## Tests
+
+Enter the following command
+
+> npm run test
+
+## Questions
+
+Please direct your questions to joneill-bootcamp
--- app.js
+++ app.js
@@ -4,32 +4,83 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+const team_array = [];
+
+// Define the add confirm inquirer array
+const add = [{
+    type: "confirm",
+    name: "add",
+    message: "Continue building team?",
+}, ];
+
+// Define the employee type inquirer questions array
+const employeeType = [{
+    type: "list",
+    name: "choice",
+    message: "Please choose the team member role",
+    choices: ["Engineer", "Intern"],
+}, ];
+
+function init() {
+
+    var theManager = new Manager();
+    console.log(theManager);
+    getDetails(theManager);
+}
+
+async function getDetails(teamMember) {
+
+    // Get Generic details commond to all Team Members
+    const teamMemberInquiry = [{
+            type: "input",
+            name: "name",
+            message: `What is the ${teamMember} name ?`,
+        },
+        {
+            type: "input",
+            name: "id",
+            message: `What is the ${teamMember.className} id ?`,
+        },
+        {
+            type: "input",
+            name: "email",
+            message: `What is the ${teamMember.className} email ?`,
+        },
+    ];
+
+    // Add role specific questions to quentionaire
+    switch (teamMember.className) {
+        case 'Manager':
+            teamMemberInquiry.push({
+                type: "input",
+                name: "phone",
+                message: `What is the Managers' office phone number ?`,
+            }, )
+            break;
+        case 'Intern':
+            teamMemberInquiry.push({
+                type: "input",
+                name: "school",
+                message: "What school does intern attend ?",
+            }, )
+            break;
+        case 'Engineer':
+            teamMemberInquiry.push({
+                type: "input",
+                name: "github",
+                message: "Please supply the Engineer's github URL ?",
+            }, )
+            break;
+    }
+
+    inquirer.prompt(teamMemberInquiry)
+        .then(console.log('done'));
+}
+
+init();
